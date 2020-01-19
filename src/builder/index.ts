c1bc40cd89c2caaa063c2427185a39ed04b33101
ning.oyn <ning.oyn@alibaba-inc.com>
2020-01-19 14:21:20 +0800

    feat(build): 完成 build 命令

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -1,8 +1,74 @@
-const dev = () => {}
+import { ConfigSchema } from '@/typings'
+import webpack, { Configuration } from 'webpack'
+import merge from 'webpack-merge'
+import WebpackDevServer from 'webpack-dev-server'
 
-const prod = () => {}
+import { mapConfigToWebpackConfig } from './webpack/transfer'
+import devConfig from './webpack/configs/dev'
+import prodConfig from './webpack/configs/prod'
+import anlzConfig from './webpack/configs/analyze'
+
+const getBuildConfig = (
+  config: ConfigSchema,
+  webpackConfig: Configuration
+): Configuration => {
+  const baseConfig = mapConfigToWebpackConfig(config)
+  return merge(baseConfig, webpackConfig)
+}
+
+const dev = (cfg: ConfigSchema): void => {
+  const config = getBuildConfig(cfg, devConfig)
+  const compiler = webpack(config)
+  const devServerOptions = Object.assign({}, config.devServer, {
+    open: true,
+    stats: {
+      colors: true
+    }
+  })
+
+  const server = new WebpackDevServer(compiler, devServerOptions)
+
+  server.listen(devServerOptions.port, () => {
+    console.log(`Starting server on http://localhost:${devServerOptions.port}`)
+  })
+}
+
+const prod = (cfg: ConfigSchema): void => {
+  const config = getBuildConfig(cfg, prodConfig)
+  const compiler = webpack(config)
+  compiler.run((err, stats) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(
+      stats.toString({
+        chunks: false, // 使构建过程更静默无输出
+        colors: true // 在控制台展示颜色
+      })
+    )
+  })
+}
+
+const analyze = (cfg: ConfigSchema): void => {
+  const config = getBuildConfig(cfg, anlzConfig)
+  const compiler = webpack(config)
+  compiler.run((err, stats) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(
+      stats.toString({
+        chunks: false, // 使构建过程更静默无输出
+        colors: true // 在控制台展示颜色
+      })
+    )
+  })
+}
 
 export default {
   dev,
-  prod
+  prod,
+  analyze
 }
---
+++ src/commands/build.ts
@@ -0,0 +1,14 @@
+import { getTJConfig } from '@/config'
+import { BuildCommandObj } from '@/typings'
+import builder from '@/builder'
+
+function build(cmdObj: BuildCommandObj): void {
+  // 先将命令行输入的参数与配置项组合
+  const baseConfig = getTJConfig()
+  if (cmdObj.input) {
+    baseConfig.input = cmdObj.input
+  }
+  builder.prod(baseConfig)
+}
+
+export default build
